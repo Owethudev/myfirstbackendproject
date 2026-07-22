8072670a1aab503781f74f905b4684319f6cf7ce
jezileowethu@gmail.com <jezileowethu@gmail.com>
2026-07-23 01:32:08 +0200

    .

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,10 +1,9 @@
 import { Router } from "express";
-import { registerUser, verifyUser, loginUser, logoutUser, updateUser, deleteUser } from "../controllers/user.controller.js";
+import { registerUser, loginUser, logoutUser, updateUser, deleteUser } from "../controllers/user.controller.js";
 
 const router = Router(); // This groups the user account paths.
 
 router.route("/register").post(registerUser);
-router.route("/verify/:token").get(verifyUser);
 router.route("/login").post(loginUser);
 router.route("/logout").post(logoutUser);
 router.route("/update").patch(updateUser);
